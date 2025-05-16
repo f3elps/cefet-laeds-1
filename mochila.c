466c9352a1e50c8da39d5ab2776085511f2b1153
dev-horizonti <desenvolvimento@horizonti.digital>
2025-05-16 16:08:46 -0300

    refactor knapsack algorithm to implement dynamic programming and improve variable naming

--- mochila.c
+++ mochila.c
@@ -1,32 +1,41 @@
 #include <stdio.h>
 
+int calculaMaximo(int a, int b) {
+    if (a < b) return b;
+    return a;
+}
+
 int calculaValorOtimo(int valorItens[], int pesoItens[], int numeroItens, int capacidadeMochila) {
     int valorOtimo = 0;
     int matrizOtima[numeroItens + 1][capacidadeMochila + 1];
 
     for (int i = 0; i < numeroItens; i++) {
         for (int j = 0; j < capacidadeMochila; j++) {
-            matrizOtima[i][j] = 0;
+            // fazer programação dinâmica:
+            // calcular qual o maior valor possível com a capacidade j e os do item 0 ate o i
+            if (i == 0 || j == 0) {
+                matrizOtima[i][j] = 0;
+            } else if (pesoItens[i-1] <= j) {
+                matrizOtima[i][j] = calculaMaximo(matrizOtima[i-1][j-1] + valorItens[i-1], matrizOtima[i-1][j]);
+            } else {
+                matrizOtima[i][j] = matrizOtima[i-1][j];
+            }
+
+            
         }
     }
 
-    for (int i = 0; i < numeroItens; i++) {
-        for (int j = 0; j < capacidadeMochila; j++) {
-
-        }
-    }
-
-    // valorOtimo = matrizOtima[numeroItens][capacidadeMochila];
+    valorOtimo = matrizOtima[numeroItens][capacidadeMochila];
     return valorOtimo;
 }
 
-int calculaValorTentativa(int solucoes[], int valorItens[], int pesoItens[], int numeroItens, int capacidadeMochila) {
+int calculaValorTentativa(int tentativa[], int valorItens[], int pesoItens[], int numeroItens, int capacidadeMochila) {
     int valorTentativa = 0;
     int pesoTotal = 0;
 
     for (int i = 0; i < numeroItens; i++) {
-        int valorTotalItem = solucoes[i] * valorItens[i];
-        int pesoTotalItem = solucoes[i] * pesoItens[i];
+        int valorTotalItem = tentativa[i] * valorItens[i];
+        int pesoTotalItem = tentativa[i] * pesoItens[i];
         valorTentativa += valorTotalItem;
         pesoTotal += pesoTotalItem;
         if (pesoTotal > capacidadeMochila) return -1;
@@ -59,6 +68,9 @@ int main () {
     }
 
     int valorOtimo = calculaValorOtimo(valorItens, pesoItens, numeroItens, capacidadeMochila);
+
+    printf("Valor otimo: %d", valorOtimo);
+    printf("Valor Tentativa: %d", valorTentativa);
 
     if (valorTentativa != valorOtimo) {
         printf("Solucao viavel mas nao otima.");
