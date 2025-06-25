d5915e28aa1413a0a49256ca30cc6f98d37d0c64
dev-horizonti <desenvolvimento@horizonti.digital>
2025-06-25 15:16:08 -0300

    add quicksort implementation and enhance list handling functions

--- projeto.c
+++ projeto.c
@@ -1,163 +1,221 @@
-/****************
-
-LAED1 - Projeto (Parte I) - Busca por padrão em sequência
-
-Alunos(as): Felipe Lara Alves Braga
-
-Data: 06/06/2025
-
-****************/
-
 #include <stdio.h>
 #include <stdlib.h>
 
-int detectaPadrao(int tipos[], int numeroSegmentos) {
-    for (int i = 0; i < numeroSegmentos - 1; i++) {
-        if (tipos[i] == 1 && tipos[i + 1] == 3 && tipos[i + 2] == 2 && tipos[i + 3] == 3 && tipos[i + 4] == 1) {
-            return 1;
+typedef int TipoChave;
+
+typedef struct {
+  int Chave;
+} TipoItem;
+
+typedef struct TipoCelula *TipoApontador;
+
+typedef struct TipoCelula {
+  TipoItem Item;
+  TipoApontador Prox;
+} TipoCelula;
+
+typedef struct {
+  TipoApontador Primeiro, Ultimo;
+} TipoLista;
+
+void FLVazia(TipoLista *Lista) {
+  Lista->Primeiro = (TipoApontador)malloc(sizeof(TipoCelula));
+  Lista->Ultimo = Lista->Primeiro;
+  Lista->Primeiro->Prox = NULL;
+}
+
+int Vazia(TipoLista Lista) {
+  return (Lista.Primeiro == Lista.Ultimo);
+}
+
+void Insere(TipoItem x, TipoLista *Lista) {
+  Lista->Ultimo->Prox = (TipoApontador)malloc(sizeof(TipoCelula));
+  Lista->Ultimo = Lista->Ultimo->Prox;
+  Lista->Ultimo->Item = x;
+  Lista->Ultimo->Prox = NULL;
+}
+
+void imprimeLista(TipoLista lista) {
+    TipoApontador p = lista.Primeiro->Prox;
+    while (p != NULL) {
+        printf("%d", p->Item.Chave);
+        if (p->Prox != NULL) {
+        printf(" ");
         }
+        p = p->Prox;
+    }
+    printf("\n");
+}
+
+
+int detectaPadrao(TipoLista *tiposDeSegmentos) {
+    TipoApontador p = tiposDeSegmentos->Primeiro->Prox;
+
+    if (Vazia(*tiposDeSegmentos) || tiposDeSegmentos->Ultimo == tiposDeSegmentos->Primeiro)
+        return 0; // lista vazia ou sem segmentos
+    // Verifica se a sequência de tipos é 1, 3, 2, 3, 1
+    while (p && p->Prox && p->Prox->Prox && p->Prox->Prox->Prox && p->Prox->Prox->Prox->Prox) {
+        int a = p->Item.Chave;
+        int b = p->Prox->Item.Chave;
+        int c = p->Prox->Prox->Item.Chave;
+        int d = p->Prox->Prox->Prox->Item.Chave;
+        int e = p->Prox->Prox->Prox->Prox->Item.Chave;
+        if (a == 1 && b == 3 && c == 2 && d == 3 && e == 1)
+        return 1;
+        p = p->Prox;
     }
     return 0;
 }
 
-void ordena (int v[],int n){
- int i, j;
- 
- for (i=0; i<n-1; i++){
-    int min = i;
-    for (j = i+1; j < n; j++){
-        if (v[j] < v[min]){
-        //troca v[min] e v[j]
-        int x = v[min];
-        v[min] = v[j];
-        v[j] = x;
+int valorJaExiste(TipoLista *lista, int valor) {
+    TipoApontador p = lista->Primeiro->Prox;
+    while (p) {
+        if (p->Item.Chave == valor)
+        return 1;
+        p = p->Prox;
     }
-    }
- }
+    return 0;
 }
 
-void contaTipoSegmento(int **matriz, int k, int vetor[], int numeroSegmentos) {
-    int segmentoAtual = 0;
-    int vetorSingular[numeroSegmentos];
-
-    for (int i = 0; i < k - 1; i++) {
-        if (vetor[i] != vetor[i + 1]) {
-            vetorSingular[segmentoAtual] = vetor[i];
-            segmentoAtual++;
+void ordenaLista(TipoLista *lista) {
+    for (TipoApontador i = lista->Primeiro->Prox; i != NULL; i = i->Prox) {
+        for (TipoApontador j = i->Prox; j != NULL; j = j->Prox) {
+        if (j->Item.Chave < i->Item.Chave) {
+            int tmp = i->Item.Chave;
+            i->Item.Chave = j->Item.Chave;
+            j->Item.Chave = tmp;
+        }
         }
     }
+}
 
-    vetorSingular[numeroSegmentos-1] = vetor[k-1];
+void contaTipoSegmento(
+    TipoLista *sequencia,
+    TipoLista *tipos
+) {
+    TipoLista unicos, ordenado;
+    FLVazia(&unicos);
+    FLVazia(&ordenado);
 
-    int vetorSingularOrdenado[numeroSegmentos];
-    int countOrdenado = 0;
+    TipoApontador p = sequencia->Primeiro->Prox;
+    TipoApontador ant = p;
+    p = p->Prox;
 
-    for (int i = 0; i < numeroSegmentos; i++) {
-        int duplicado = 0;
-        for (int j = 0; j < countOrdenado; j++) {
-            if (vetorSingular[i] == vetorSingularOrdenado[j]) {
-                duplicado = 1;
-                break;
+    // Transforma o vetor em um vetor Singular, ou seja, cada segmento possui apenas um representante
+    while (p != NULL) {
+        if (p->Item.Chave != ant->Item.Chave) {
+            if (!valorJaExiste(&unicos, ant->Item.Chave)) {
+                Insere(ant->Item, &unicos);
             }
+            ant = p;
         }
-        if (!duplicado) {
-            vetorSingularOrdenado[countOrdenado++] = vetorSingular[i];
-        }
+        p = p->Prox;
     }
 
-    ordena(vetorSingularOrdenado, countOrdenado);
+    if (!valorJaExiste(&unicos, ant->Item.Chave)) {
+        Insere(ant->Item, &unicos);
+    }
 
-    for (int i = 0; i < numeroSegmentos; i++) {
-        for (int j = 0; j < numeroSegmentos; j++) {
-            if (vetorSingular[i] == vetorSingularOrdenado[j]) {
-                matriz[0][i] = j + 1;
-                break;
+    // Depois de transformar o vetor em singular, ordena ele
+    // O vetor ordenado servirá como um guia para atribuir o tipo para o segmento
+    TipoApontador q = unicos.Primeiro->Prox;
+    while (q != NULL) {
+        Insere(q->Item, &ordenado);
+        q = q->Prox;
+    }
+    ordenaLista(&ordenado);
+
+    
+    p = sequencia->Primeiro->Prox;
+    ant = p;
+    p = p->Prox;
+
+    while (p != NULL) {
+        if (p->Item.Chave != ant->Item.Chave) {
+            // descobre o tipo
+            int tipo = 1;
+            TipoApontador r = ordenado.Primeiro->Prox;
+            while (r != NULL) {
+                if (r->Item.Chave == ant->Item.Chave) break;
+                tipo++;
+                r = r->Prox;
             }
+            TipoItem tipoItem;
+            tipoItem.Chave = tipo;
+            Insere(tipoItem, tipos);
+            ant = p;
         }
+        p = p->Prox;
     }
+
+    // último segmento
+    int tipo = 1;
+    TipoApontador r = ordenado.Primeiro->Prox;
+    while (r != NULL) {
+        if (r->Item.Chave == ant->Item.Chave) break;
+        tipo++;
+        r = r->Prox;
+    }
+    TipoItem tipoItem;
+    tipoItem.Chave = tipo;
+    Insere(tipoItem, tipos);
 }
 
-void contarElementosSegmento(int **matriz, int k, int vetor[]) {
-    int segmentoAtual = 0;
-    for (int i = 0; i < k - 1; i++) {
-        if (vetor[i] != vetor[i + 1]) {
-            segmentoAtual++;
-        } else {
-            matriz[1][segmentoAtual]++;
-        }
-    }
-}
-
-int main () {
-    int **matriz, k;
-
+int main() {
+    // K é a quantidade de pixels e L é a quantidade de linhas
+    int k, valor, l;
+    scanf("%d", &l);
     scanf("%d", &k);
 
-    int vetor[k];
+    TipoLista sequencia, linha, encontrouSequenciaLinha, tiposSegmentosLinha;
+    FLVazia(&sequencia);
+    FLVazia(&encontrouSequenciaLinha);
+    FLVazia(&tiposSegmentosLinha);
 
-    matriz = (int **)malloc(2 * sizeof(int *));
-
-
+    // Preenche a lista com k elementos
     for (int i = 0; i < k; i++) {
-        scanf("%d", &vetor[i]);
+        scanf("%d", &valor);
+        TipoItem item;
+        item.Chave = valor;
+        Insere(item, &sequencia);
     }
-
-    int numeroSegmentos = 0;
-
-    for (int i = 0; i < k - 1; i++) {
-        if (vetor[i] != vetor[i + 1]) {
-            numeroSegmentos++;
-        }
-    }
-    // O primeiro segmento sempre existe
-    numeroSegmentos++;
     
-    int vetorSingular[numeroSegmentos];
+    // Separa as linhas e, para cada linha, verifica se o padrão foi encontrado e armazena o resultado na lista encontrouSequenciaLinha
+    TipoApontador atual = sequencia.Primeiro->Prox;
 
-    for (int i = 0; i < 2; i ++) {
-        matriz[i] = (int *) malloc(numeroSegmentos * sizeof(int));
-    }
+    for(int i = 0; i < k / l; i++) {
+        FLVazia(&linha);
+        FLVazia(&tiposSegmentosLinha); // reinicializa para cada linha
 
-    for (int i = 0; i < 2; i++) {
-        for(int j = 0; j < numeroSegmentos; j++) {
-            matriz[i][j] = 1;
+        for (int j = 0; j < l && atual != NULL; j++) {
+            Insere(atual->Item, &linha);
+            atual = atual->Prox;
         }
+
+        contaTipoSegmento(&linha, &tiposSegmentosLinha);
+        int padrao = detectaPadrao(&tiposSegmentosLinha);
+        TipoItem item;
+        item.Chave = padrao;
+        Insere(item, &encontrouSequenciaLinha);
     }
 
-    contarElementosSegmento(matriz, k, vetor);
 
-    int vetorOrdenado[k];
+    imprimeLista(encontrouSequenciaLinha);
 
-    for (int i = 0; i < k; i++) {
-        vetorOrdenado[i] = vetor[i];
-    }
+    // Análise de segmentos
+    TipoLista segmentos, segmentosOrdenados, tipos;
+    FLVazia(&segmentos);
+    FLVazia(&segmentosOrdenados);
+    FLVazia(&tipos);
 
-    ordena(vetorOrdenado, k);
+    contaTipoSegmento(&sequencia, &tipos);
 
-    contaTipoSegmento(matriz, k, vetor, numeroSegmentos);
-
-    int padraoDetectado = detectaPadrao(matriz[0], numeroSegmentos);
-
-    if (padraoDetectado == 1) {
+    int padrao = detectaPadrao(&tipos);
+    if (padrao) {
         printf("Resultado: Padrao encontrado.\n");
     } else {
         printf("Resultado: Padrao nao encontrado.\n");
     }
-    
-    // for (int j = 0; j < 2; j++) {
-    //     for (int i = 0; i < numeroSegmentos; i++) {
-    //         if (i == numeroSegmentos - 1) {
-    //             printf("%d", matriz[j][i]);
-    //         } else {
-    //             printf("%d ", matriz[j][i]);
-    //         }
-    //     }
-    //     printf("\n");
-    // }
 
-    for (int i = 0; i < 2; i++) {
-        free(matriz[i]);
-    }
-
-    free(matriz);
+    return 0;
 }
